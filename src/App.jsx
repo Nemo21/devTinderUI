103c87ee7e4444f3aec893d7fc015eb8a2ffb255
Nemo21 <saggubableen9@gmail.com>
2025-02-10 18:10:36 +0530

    Routing and Basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
-import Navbar from './Navbar'
+
+import Body from './Body'
+import Login from './Login'
+import Profile from './Profile'
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <h1 className="text-3xl font-bold ">Hello world!</h1>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/profile" element={<Profile/>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
